b9a56c61eb16ff2f15a05f0a3abdf1de896f233a
dariwan <dariwan1607@gmail.com>
2023-10-11 15:56:15 +0700

    adding home menu

---
+++ app/src/main/java/com/dariwan/petanimaju/model/ProductModel.kt
@@ -0,0 +1,7 @@
+package com.dariwan.petanimaju.model
+
+data class ProductModel (
+    val image: Int,
+    val title: String,
+    val harga: Int,
+)
--- app/src/main/java/com/dariwan/petanimaju/view/home/HomeFragment.kt
+++ app/src/main/java/com/dariwan/petanimaju/view/home/HomeFragment.kt
@@ -5,56 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dariwan.petanimaju.R
+import com.dariwan.petanimaju.adapter.FlashSaleAdapter
+import com.dariwan.petanimaju.databinding.FragmentHomeBinding
+import com.dariwan.petanimaju.model.ProductModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAction()
     }
+
+    private fun setupAction() {
+        binding.rvHome.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        val dataList: MutableList<ProductModel> = mutableListOf()
+        val flashSaleTitle: Array<String> = resources.getStringArray(R.array.title_home)
+        val flashSalePrice: IntArray = resources.getIntArray(R.array.price_home)
+
+        val flashSaleImage : List<Int> = listOf(
+            R.drawable.img_product_1,
+            R.drawable.img_product_2,
+            R.drawable.img_product_3,
+            R.drawable.img_product_4,
+        )
+
+        flashSaleTitle.forEachIndexed { index, name ->
+            dataList.add(ProductModel(flashSaleImage.get(index), name, flashSalePrice[index]))
+        }
+
+        val adapter = FlashSaleAdapter(dataList)
+        binding.rvHome.adapter = adapter
+    }
+
 }
